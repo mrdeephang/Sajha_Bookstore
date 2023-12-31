21a5dad062983c59f71a0c72886e400e7302336e
Deephang Thegim <thoklihang.deep@gmail.com>
2023-12-31 22:41:21 +0545

    adding theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:sajhabackup/EasyConst/style.dart';
 import 'package:sajhabackup/splashs/splashscreen.dart';
-//import 'package:sajhabackup/splashs/splashscreen.dart';
-//import 'package:sajhabackup/splashs/splashscreen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: splashscreen(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF9526BC)),
+        fontFamily: regular,
+      ),
+      home: splashscreen(),
+    );
+  }
 }
