f218a8abd2461e8483fe85388e23c91e5a1d089c
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-01-03 17:47:16 +0545

    esp-chat

---
+++ lib/Chat/chat.dart
@@ -0,0 +1,62 @@
+//import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:sajhabackup/pages/chatpage.dart';
+
+class chat extends StatefulWidget {
+  const chat({super.key});
+
+  @override
+  State<chat> createState() => _chatState();
+}
+
+class _chatState extends State<chat> {
+   final FirebaseAuth _auth=FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('CHAT',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+        leading: BackButton(),
+      ),
+      body: _buildUserList(),
+    );
+  }
+  
+  Widget _buildUserList(){
+    return StreamBuilder<QuerySnapshot>(stream:FirebaseFirestore.instance.collection('users').snapshots(),
+    builder: (context,snapshot){
+      if(snapshot.hasError){
+        return Text('error');
+      }
+      if(snapshot.connectionState==ConnectionState.waiting){
+        return Text('Loading..');
+      }
+      return ListView(
+        children: snapshot.data!.docs.map<Widget>((doc)=>_buildUserListItem(doc)).toList(),
+      );
+    },
+    );
+   
+  }
+   Widget _buildUserListItem(DocumentSnapshot document){
+      Map<String, dynamic> data = document.data()! as Map<String,dynamic>;
+      if(_auth.currentUser!.email !=data['Email']){
+        return ListTile(
+          title: Text(data['Email']),
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>chatpage(
+              receiveruserEmail: data['Email'],
+              receiverId: data['Full Name'],
+            )));
+          },
+        );
+      }else{
+        return Container();
+      }
+    }
+}
