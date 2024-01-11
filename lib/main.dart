3c1f4fc98ce6510f9c4720ed8ec1f24fc36da5c8
Deephang Thegim <thoklihang.deep@gmail.com>
2024-01-11 10:48:13 +0545

    Color

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sajhabackup/Splashes/splashscreen.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Platform.isAndroid
