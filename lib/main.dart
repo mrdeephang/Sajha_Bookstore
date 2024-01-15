c98fa4cb7c83fb97ccca98c8cd648d25d0885fc9
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-01-15 15:41:37 +0545

    smth

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sajhabackup/HomePage/homepage.dart';
 import 'package:sajhabackup/Splashes/splashscreen.dart';
 import 'package:sajhabackup/pages/cartmodel.dart';
+import 'package:sajhabackup/themes/themeprovider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +17,22 @@ Future<void> main() async {
               projectId: 'sajhabookstore',
               storageBucket: 'gs://sajhabookstore.appspot.com'))
       : await Firebase.initializeApp();
-  runApp(ChangeNotifierProvider(
-    create: (context) => CartModel(),
-    child: MyApp(),
-  ));
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartModel()),
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
+      child: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -29,7 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     // theme: Provider.of<ThemeProvider>(context).themData,
+      theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
       home: splashscreen(),
     );
