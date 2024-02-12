5ac8b1d4cc339457970a4f06a29f2934558512aa
Deephang Thegim <132187755+ninjaaaxxx@users.noreply.github.com>
2024-02-12 22:24:39 +0545

    Google Sign In

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -1,22 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sajhabackup/HomePage/homepage.dart';
 
-class AuthService{
-  final FirebaseAuth _auth=FirebaseAuth.instance;
-  final FirebaseFirestore _firestore=FirebaseFirestore.instance;
-  User? getCurrentUser(){
-    return _auth.currentUser;
-  }
-  Future<UserCredential> signInWithEmailPassword(String email, String password) async{
-    try{
-      UserCredential userCredential=await _auth.signInWithEmailAndPassword(email: email, password: password);
-      _firestore.collection("users").doc(userCredential.user!.uid).set({
-        'uid':userCredential.user!.uid,'Email':email
-      });
-      return userCredential;
-    }on FirebaseAuthException catch(e){
-      throw Exception(e.code);
+class FirebaseService {
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  static Future<String?> signInwithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => homepage()));
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+      throw e;
     }
   }
-  
+
+  Future<void> signOutFromGoogle() async {
+    await _googleSignIn.signOut();
+    await _auth.signOut();
+  }
 }
